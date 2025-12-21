bf819b4defe250c0894fc73a139b9c5be5254b27
Euan <euan.m.campbell@protonmail.com>
2025-12-21 18:52:36 +0000

    added support for HF and PostgreSQL

--- src/finetl/loading/__init__.py
+++ src/finetl/loading/__init__.py
@@ -1,7 +1,16 @@
 """Loading module for FinETL."""
 
 from finetl.loading.csv import CSVLoader
+from finetl.loading.huggingface import HuggingFaceLoader
 from finetl.loading.parquet import ParquetLoader
+from finetl.loading.postgresql import PostgreSQLLoader
 from finetl.loading.registry import get_loader, register_loader
 
-__all__ = ["CSVLoader", "ParquetLoader", "get_loader", "register_loader"]
+__all__ = [
+    "CSVLoader",
+    "HuggingFaceLoader",
+    "ParquetLoader",
+    "PostgreSQLLoader",
+    "get_loader",
+    "register_loader",
+]
